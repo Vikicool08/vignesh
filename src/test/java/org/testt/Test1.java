6c0f50cad8b7e7aa0c1db1c9fa50587061cdfd03
Vikicool08 <vikicool08@gmail.com>
2021-04-28 14:23:12 +0530

    merging

--- src/test/java/org/testt/Test1.java
+++ src/test/java/org/testt/Test1.java
@@ -13,4 +13,28 @@ public class Test1 {
 	private void tc12() {
 		System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
 	}
+	@Test
+	private void tc15() {
+	System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
+	@Test
+	private void tc18() {
+		System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
+	@Test
+	private void tc19() {
+		System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
+	@Test
+	private void tc14() {
+	System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
+	@Test
+	private void tc16() {
+		System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
+	@Test
+	private void tc17() {
+		System.out.println("Test 1 Thread count:"+Thread.currentThread().getId());
+	}
 }
